9708b7c80875fdc2667c43f9bae2b102208ccfc7
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2025-02-22 05:21:23 +0200

    feat!: add new bool methods

--- lib/core/shared_preference.dart
+++ lib/core/shared_preference.dart
@@ -22,6 +22,19 @@ class SharedPreference {
     return _shared!.getString(key);
   }
 
+// Save a string value
+  static Future<bool> setBool(
+      {required String key, required bool value}) async {
+    if (_shared == null) throw Exception('SharedPreferences not initialized');
+    return _shared!.setBool(key, value);
+  }
+
+  // Retrieve a string value
+  static bool? getBool({required String key}) {
+    if (_shared == null) throw Exception('SharedPreferences not initialized');
+    return _shared!.getBool(key);
+  }
+
   // Remove a value
   static Future<bool> remove({required String key}) async {
     if (_shared == null) throw Exception('SharedPreferences not initialized');
